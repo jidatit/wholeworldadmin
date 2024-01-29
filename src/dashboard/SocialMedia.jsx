795a365aa4a0aefe7542c2a256b161685defc30f
Etzshally <shahpaal10@hotmail.com>
2024-01-29 13:17:30 +0500

    `admin backend`

--- src/dashboard/SocialMedia.jsx
+++ src/dashboard/SocialMedia.jsx
@@ -1,32 +1,108 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { TextField, FormControl, InputLabel, Select, MenuItem } from '@mui/material'
 import Button from "../components/Button"
+import { addDoc, collection } from "firebase/firestore";
+import { db } from '../../firebase';
+import { storage } from '../../firebase';
+import { ref, uploadBytes, getDownloadURL } from '@firebase/storage';
+import { toast, ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 const SocialMedia = () => {
+
+    const [formData, setFormData] = useState({
+        title: '',
+        category: '',
+        description: '',
+        imagesUrl: [],
+        imagePreviews: [],
+    });
+
+    const handleFileChange = (event) => {
+        const files = Array.from(event.target.files);
+        const imagePreviews = [];
+
+        files.forEach((file) => {
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                imagePreviews.push(e.target.result);
+                setFormData((prevData) => ({
+                    ...prevData,
+                    imagePreviews: [...imagePreviews],
+                }));
+            };
+            reader.readAsDataURL(file);
+        });
+
+        setFormData((prevData) => ({
+            ...prevData,
+            photos_field: files,
+        }));
+    };
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    const handleSubmit = async () => {
+        let imagesUrl = [];
+        try {
+            await Promise.all(
+                formData.photos_field.map(async (photo) => {
+                    const storageRef = ref(storage, `admin_socialmedia_posts/${photo.name}`);
+                    await uploadBytes(storageRef, photo);
+                    const downloadURL = await getDownloadURL(storageRef);
+                    imagesUrl.push(downloadURL);
+                })
+            );
+
+            const postData = {
+                title: formData.title,
+                category: formData.category,
+                description: formData.description,
+                imagesUrl: imagesUrl,
+            };
+
+            await addDoc(collection(db, 'admin_socialmedia_posts'), postData);
+            console.log("Document successfully written to Firestore.");
+            toast.success("Success !", {
+                position: "top-center"
+            });
+        } catch (error) {
+            console.error("Error writing document to Firestore:", error);
+            toast.error("Error !", {
+                position: "top-center"
+            });
+        }
+    };
+
     return (
         <div className='w-full flex flex-col bg-[#FAFAFA] justify-center items-center'>
-
+            <ToastContainer />
             <div className='w-[90%] flex flex-col justify-center items-start'>
                 <h1 className='text-black font-bold text-[25px] mt-5 mb-5'>Social Media Posts</h1>
             </div>
 
-            <div className='w-[90%] gap-4 h-[500px] bg-white flex flex-col justify-center items-center rounded-md shadow-lg'>
-                <TextField label="Enter Title of Post" type='text' className='w-[70%]' />
+            <div className='w-[90%] gap-4 h-[600px] bg-white flex flex-col justify-center items-center rounded-md shadow-lg'>
+                <TextField label="Enter Title of Post" onChange={handleChange} value={formData.title} name='title' type='text' className='w-[70%]' />
                 <FormControl className='w-[70%]'>
                     <InputLabel id="demo-simple-select-label">Select Category of Post</InputLabel>
                     <Select
                         labelId="demo-simple-select-label"
                         id="demo-simple-select"
-                        // value={category}
                         label="Select Category of Item"
-                    // onChange={handleChange}
+                        onChange={handleChange} value={formData.category} name='category'
                     >
                         <MenuItem value={10}>Ten</MenuItem>
                         <MenuItem value={20}>Twenty</MenuItem>
                         <MenuItem value={30}>Thirty</MenuItem>
                     </Select>
                 </FormControl>
-                <TextField label="Description of Post" type='text' className='w-[70%]' />
+                <TextField label="Description of Post" type='text' onChange={handleChange} value={formData.description} name='description' className='w-[70%]' />
                 <div className="flex items-center justify-center w-[70%]">
                     <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-34 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-white-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
                         <div className="flex flex-col items-center justify-center pt-5 pb-6">
@@ -36,12 +112,19 @@ const SocialMedia = () => {
                             <p className="mb-2 text-sm text-gray-500 text-center dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
                             <p className="text-xs text-gray-500 text-center dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
                         </div>
-                        <input id="dropzone-file" type="file" className="hidden" />
+                        <input id="dropzone-file" type="file" onChange={handleFileChange} value={formData.imagesUrl} name='imagesUrl' multiple className="hidden" />
                     </label>
                 </div>
+
+                <div className='w-full flex flex-row justify-center items-center'>
+                    {formData.imagePreviews.length > 0 && formData.imagePreviews.map((preview, index) => (
+                        <img key={index} src={preview} alt={`Thumbnail ${index}`} className="w-10 h-10 m-2 object-cover rounded-lg" />
+                    ))}
+                </div>
+
                 <div className='w-[90%] mb-5 flex flex-col justify-end items-end'>
                     <div className='md:w-[30%] w-full pr-0 md:pr-2'>
-                        <Button text="Publish" />
+                        <Button onClickProp={handleSubmit} text="Publish" />
                     </div>
                 </div>
             </div>
