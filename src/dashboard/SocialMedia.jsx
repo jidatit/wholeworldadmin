ba9b5b5245ee445327f10926e9484e86caeec50f
Etzshally <shahpaal10@hotmail.com>
2024-01-31 14:03:50 +0500

    `video`

--- src/dashboard/SocialMedia.jsx
+++ src/dashboard/SocialMedia.jsx
@@ -9,7 +9,6 @@ import { ref, uploadBytes, getDownloadURL, deleteObject } from '@firebase/storag
 
 const SocialMedia = () => {
     const [entity, setEntity] = useState({});
-    const [thirdImageUrl, setThirdImageUrl] = useState('');
     const [updatedVideo, setUpdatedVideo] = useState(null);
 
     useEffect(() => {
@@ -24,10 +23,6 @@ const SocialMedia = () => {
                     };
                 });
                 setEntity(data);
-                if (data.imagesUrl && data.imagesUrl.length >= 4) {
-                    setThirdImageUrl(data.imagesUrl[3]);
-                    data.imagesUrl.splice(3, 1);
-                }
             } catch (error) {
                 toast.error(error.message);
             }
@@ -48,11 +43,11 @@ const SocialMedia = () => {
             const updatedVideoDownloadUrl = await getDownloadURL(storageRef);
 
             const updatedImagesUrl = [...entity.imagesUrl];
-            updatedImagesUrl[3] = updatedVideoDownloadUrl;
 
             await setDoc(doc(db, "admin_socialmedia_posts", entity.id), {
                 imagesUrl: updatedImagesUrl,
-                title: entity.title
+                title: entity.title,
+                videoUrl: updatedVideoDownloadUrl
             });
 
             toast.success("Video updated successfully!");
@@ -63,7 +58,7 @@ const SocialMedia = () => {
 
     const handleVideoDelete = async () => {
         try {
-            const videoUrl = thirdImageUrl;
+            const videoUrl = entity.videoUrl;
             if (!videoUrl) {
                 toast.error("No video found to delete.");
                 return;
@@ -71,13 +66,12 @@ const SocialMedia = () => {
 
             const storageRef = ref(storage, `admin_socialmedia_posts/updated_video.mp4`);
             await deleteObject(storageRef);
-
             const updatedImagesUrl = [...entity.imagesUrl];
-            updatedImagesUrl[3] = '';
 
             await setDoc(doc(db, "admin_socialmedia_posts", entity.id), {
                 imagesUrl: updatedImagesUrl,
-                title: entity.title
+                title: entity.title,
+                videoUrl:''
             });
 
             toast.success("Video deleted successfully!");
@@ -91,6 +85,22 @@ const SocialMedia = () => {
         setUpdatedVideo(file);
     };
 
+    const updateImage = async (index,imageUrl) => {
+        try {
+
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+    const deleteImage = async (index,imageUrl) => {
+        try {
+
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
     return (
         <div className='w-full flex flex-col bg-[#FAFAFA] justify-center items-center'>
             <ToastContainer />
@@ -102,15 +112,15 @@ const SocialMedia = () => {
                 <TextField className='md:w-[75%]' label="Enter Title" />
                 <div className='w-full flex flex-row justify-center gap-3 items-center flex-wrap'>
 
-                    {entity.imagesUrl && entity.imagesUrl?.slice(0, 3).map((imageUrl, index) => (
+                    {entity.imagesUrl && entity.imagesUrl?.map((imageUrl, index) => (
                         <div key={index} className='relative rounded-md w-full sm:w-[50%] md:w-[33%] lg:w-[25%] max-h-[200px]'>
                             <img className='w-full max-h-[200px] rounded-md' src={imageUrl} alt="" />
-                            <button onClick={() => updateImage(imageUrl)} className='absolute top-[20%] sm:top-[15%] md:top-[20%] bg-blue-500 right-[40%] sm:right-[35%] md:right-[40%] text-white font-semibold px-5 py-3 rounded-lg'>
+                            <button onClick={() => updateImage(index, imageUrl)} className='absolute top-[20%] sm:top-[15%] md:top-[20%] bg-blue-500 right-[40%] sm:right-[35%] md:right-[40%] text-white font-semibold px-5 py-3 rounded-lg'>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
                                 </svg>
                             </button>
-                            <button onClick={() => deleteImage(imageUrl)} className='absolute top-[55%] sm:top-[70%] md:top-[55%] bg-red-500 text-white font-semibold right-[40%] sm:right-[35%] md:right-[40%] px-5 py-3 rounded-lg'>
+                            <button onClick={() => deleteImage(index, imageUrl)} className='absolute top-[55%] sm:top-[70%] md:top-[55%] bg-red-500 text-white font-semibold right-[40%] sm:right-[35%] md:right-[40%] px-5 py-3 rounded-lg'>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                 </svg>
@@ -120,9 +130,9 @@ const SocialMedia = () => {
 
                 </div>
 
-                {thirdImageUrl ? (
+                {entity.videoUrl ? (
                     <video className="w-full md:w-[75%] h-[200px]" controls>
-                        <source src={thirdImageUrl} type="video/mp4" />
+                        <source src={entity.videoUrl} type="video/mp4" />
                         Your browser does not support the video tag.
                     </video>
                 ) : (
